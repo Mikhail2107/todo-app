4f1f5bfac9258fe478c7e7d14a0b94b4f38d9f8b
Mikhail2107 <mixa.proshin@yandex.ru>
2025-04-04 19:52:23 +0300

    delete imports

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }], // Для Node.js
-    '@babel/preset-react', // Для JSX
+    ['@babel/preset-react', { runtime: 'automatic' }], // Для JSX
     '@babel/preset-typescript', // Для TypeScript
   ],
 };
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useRef, useState } from 'react';
 import Filters from '../Filters/Filters';
 import TodoList from '../TodoList/TodoList';
